3cdd55b2c04168422115adf00999b0cf6523e55e
Manish Garg <manish@Manishs-MacBook-Air.local>
2025-03-16 12:24:09 +0530

    Add whatsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
 /* eslint-disable react/forbid-prop-types */
 import { Preloader } from "@/components/common";
 import PropType from "prop-types";
-import React, { StrictMode } from "react";
+import React, { StrictMode, useEffect } from "react";
 import { Provider } from "react-redux";
+import { Helmet } from "react-helmet";
 import { PersistGate } from "redux-persist/integration/react";
 import AppRouter from "@/routers/AppRouter";
+import { waitForGlobal } from "@/helpers/utils";
 
-const App = ({ store, persistor }) => (
-  <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<Preloader />} persistor={persistor}>
-        <AppRouter />
-      </PersistGate>
-    </Provider>
-  </StrictMode>
-);
+const wa_btnSetting = {
+  btnColor: "#0d9488",
+  ctaText: "WhatsApp Us",
+  cornerRadius: 40,
+  marginBottom: 20,
+  marginLeft: 20,
+  marginRight: 20,
+  btnPosition: "left",
+  whatsAppNumber: "919032477570",
+  welcomeMessage: "Hello, Can you please help me with more information.",
+  zIndex: 999999,
+  btnColorScheme: "light",
+};
+
+const App = ({ store, persistor }) => {
+  useEffect(() => {
+    waitForGlobal("_waEmbed").then(() => {
+      _waEmbed(wa_btnSetting);
+    });
+  }, []);
+
+  return (
+    <StrictMode>
+      <Provider store={store}>
+        <Helmet>
+          <script
+            async
+            src="https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js"
+          ></script>
+        </Helmet>
+        <PersistGate loading={<Preloader />} persistor={persistor}>
+          <AppRouter />
+        </PersistGate>
+      </Provider>
+    </StrictMode>
+  );
+};
 
 App.propTypes = {
   store: PropType.any.isRequired,
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -74,3 +74,26 @@ export const displayActionMessage = (msg, status = "info") => {
     }
   }, 3000);
 };
+
+export function waitForGlobal(name, timeout = 300) {
+  return new Promise((resolve, reject) => {
+    let waited = 0;
+
+    function wait(interval) {
+      setTimeout(() => {
+        waited += interval;
+        // some logic to check if script is loaded
+        // usually it something global in window object
+        if (window[name] !== undefined) {
+          return resolve();
+        }
+        if (waited >= timeout * 1000) {
+          return reject({ message: "Timeout" });
+        }
+        wait(interval * 2);
+      }, interval);
+    }
+
+    wait(30);
+  });
+}
