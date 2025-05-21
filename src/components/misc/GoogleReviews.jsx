3caf8f5fd983cfa77dd282a679656bf5ebe8b259
Manish Garg <manish@Manishs-Air.bbrouter>
2025-05-21 23:12:35 +0530

    Google Reviews

---
+++ src/components/misc/GoogleReviews.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { Card, Spin } from "antd";
+import { ReactGoogleReviews } from "react-google-reviews";
+import "react-google-reviews/dist/index.css";
+
+const GoogleReviews = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await fetch("/api/getGoogleReviews", {
+          method: "GET",
+          headers: {
+            "X-Api-Key": "API_KEY",
+          },
+        });
+        const data = await response.json();
+        if (data.body.result && data.body.result.reviews) {
+          setReviews(
+            data.body.result.reviews.map((r, index) => ({
+              reviewId: index,
+              reviewer: {
+                profilePhotoUrl: r.profile_photo_url,
+                displayName: r.author_name,
+              },
+              starRating: r.rating,
+              comment: r.text,
+              createTime: r.time,
+            }))
+          );
+        } else {
+          console.error("No reviews found");
+        }
+      } catch (error) {
+        console.error("Error fetching reviews:", error);
+      }
+    };
+
+    fetchReviews();
+  }, []);
+
+  return (
+    <div>
+      <Card
+        title="Customer Testimonials"
+        styles={{
+          header: {
+            backgroundColor: "rgb(13, 148, 136)",
+            color: "#fff",
+            textAlign: "center",
+            fontWeight: "600",
+            fontSize: "1.6rem",
+          },
+        }}
+      >
+        {reviews.length > 0 ? (
+          <div className="google-reviews-carousel">
+            {" "}
+            <ReactGoogleReviews layout="carousel" reviews={reviews} />
+          </div>
+        ) : (
+          <div
+            className="ezys-spinner"
+            style={{ height: "unset", marginTop: "5rem" }}
+          >
+            <Spin size="large" />
+          </div>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default GoogleReviews;
