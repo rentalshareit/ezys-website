76a039af057b2a2cd88c5759c0f9ebc2393e686d
Manish Garg <manish@Manishs-MacBook-Air.local>
2025-07-02 23:18:38 +0530

    Skeleton on home page

---
+++ src/constants/skeleton.js
@@ -0,0 +1,28 @@
+export const productsSkeleton = {
+  category1: [
+    { skeleton: true, id: 1 },
+    { skeleton: true, id: 2 },
+    { skeleton: true, id: 3 },
+    { skeleton: true, id: 4 },
+    { skeleton: true, id: 5 },
+  ],
+  category2: [
+    { skeleton: true, id: 6 },
+    { skeleton: true, id: 7 },
+    { skeleton: true, id: 8 },
+    { skeleton: true, id: 9 },
+  ],
+  category3: [
+    { skeleton: true, id: 10 },
+    { skeleton: true, id: 11 },
+    { skeleton: true, id: 12 },
+    { skeleton: true, id: 13 },
+    { skeleton: true, id: 14 },
+  ],
+  category4: [
+    { skeleton: true, id: 15 },
+    { skeleton: true, id: 16 },
+    { skeleton: true, id: 17 },
+    { skeleton: true, id: 18 },
+  ],
+};
