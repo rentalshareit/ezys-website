0bc5107f811ef8c4b0f5ef01ed059cc144472914
Manish Garg <manish@Manishs-MacBook-Air.local>
2025-10-03 17:55:01 +0530

    Basket Changes - Shorten oos message, label on date range

--- src/hooks/useRefreshOnISTDateChangeMoment.js
+++ src/hooks/useRefreshOnISTDateChangeMoment.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import moment from "moment";
+import moment from "moment-timezone";
 
 function useRefreshOnISTDateChange() {
   const getISTDate = () => {
